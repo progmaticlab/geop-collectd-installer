88582faeb1e6b775b26fdba9f5f8a81b0a3a79c3
Arina Argentova <aaalt@kel.as>
2021-02-11 13:12:38 +0300

    [~] ssd_disk_size -> disk_size_ssd

--- playbooks/roles/collectd/files/vm_disk.py
+++ playbooks/roles/collectd/files/vm_disk.py
@@ -35,7 +35,7 @@ def read_func():
     MB = float(1024**2)
     ignore_fs = ["tmpfs", "devtmpfs", "devfs", "iso9660", "overlay", "aufs", "squashfs",
                 "cgroup", "cgroup2", "autofs", "proc", "sysfs", "bpf", "devpts", "securityfs",
-                "pstore", "efivarfs", "hugetlbfs", "mqueue", "debugfs", "tracefs", "fusectl", 
+                "pstore", "efivarfs", "hugetlbfs", "mqueue", "debugfs", "tracefs", "fusectl",
                 "configfs", "binfmt_misc", "fuse.gvfsd-fuse", "fuse.lxcfs", "nsfs"]
 
     disk_type_map_path = '/opt/collectd_plugins/dict/disk_type_map.json'
@@ -59,14 +59,14 @@ def read_func():
         disks_data = {}
         for line in f.read().split('\n'):
             fs_data_array = line.split()
-            
+
             if not len(fs_data_array) or fs_data_array[2] in ignore_fs:
                 continue
 
             fs_data_struct = os.statvfs(fs_data_array[1])
             fs_size = float(fs_data_struct.f_bsize * fs_data_struct.f_blocks) / MB
             fs_usage = float(fs_data_struct.f_bsize) * float(fs_data_struct.f_blocks - fs_data_struct.f_bfree) / MB
-                
+
             disk_name = re.findall(r'[a-z]+', fs_data_array[0])[-1]
 
             for key in disk_type_map.keys():
@@ -89,12 +89,12 @@ def read_func():
         # vm disk fs size
         collectd.Values(plugin = 'vm_disk',
                     type_instance = disk,
-                    type = cur_disk_data.get('type') + '_disk_size',
+                    type = 'disk_size_' + cur_disk_data.get('type'),
                     values = [truncate(cur_disk_data.get('disk_size'), 6)]).dispatch()
         # vm disk fs usage
         collectd.Values(plugin = 'vm_disk',
                     type_instance = disk,
-                    type = cur_disk_data.get('type') + '_disk_usage',
+                    type = 'disk_usage_' + cur_disk_data.get('type'),
                     values = [truncate(cur_disk_data.get('disk_usage'), 6)]).dispatch()
 
 
